0f8698818f773cb255aaf3278d4682c00ae77fc7
LoveBosikova <117667170+LoveBosikova@users.noreply.github.com>
2023-04-01 21:57:40 +0300

    JavaScript pointers DONE

--- assets/style/pointers.js
+++
@@ -1,8 +0,0 @@
-@import "difference.css"
-
-var rightpointer = document.querySelector("pointers__rightside");
-var boeingItems = document.querySelector("difference__boeing-items");
-
-rightpointer.addEventListener("click", function () {
-    boeingItems.style.display = boeingItems.style.display == "block" ? "none" : "block";
-});
